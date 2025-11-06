e9b053ba8aed728455679df3914515d927959885
L̷O̷N̷D̷O̷N̷少林 <22037550+Shivawkes@users.noreply.github.com>
2025-11-06 10:24:04 -0500

    Translate Japanese comments to English

--- src/module.ts
+++ src/module.ts
@@ -30,11 +30,11 @@ export default abstract class Module {
 	}
 
 	/**
-	 * コンテキストを生成し、ユーザーからの返信を待ち受けます
-	 * @param key コンテキストを識別するためのキー
-	 * @param isChat チャット上のコンテキストかどうか
-	 * @param id チャット上のコンテキストならばチャット相手のID、そうでないなら待ち受ける投稿のID
-	 * @param data コンテキストに保存するオプションのデータ
+	 * Creates a context and waits for a user reply.
+		* @param key: The key to identify the context.
+		* @param isChat: Is this a chat context?
+		* @param id: If this is a chat context, the chat partner's ID. If not, the post ID to wait for.
+		* @param data: Optional data to store in the context.
 	 */
 	@bindThis
 	protected subscribeReply(key: string | null, isChat: boolean, id: string, data?: any) {
@@ -42,8 +42,8 @@ export default abstract class Module {
 	}
 
 	/**
-	 * 返信の待ち受けを解除します
-	 * @param key コンテキストを識別するためのキー
+	 * Cancels reply waiting.
+	 * @param key A key to identify the context.
 	 */
 	@bindThis
 	protected unsubscribeReply(key: string | null) {
@@ -51,10 +51,10 @@ export default abstract class Module {
 	}
 
 	/**
-	 * 指定したミリ秒経過後に、タイムアウトコールバックを呼び出します。
-	 * このタイマーは記憶に永続化されるので、途中でプロセスを再起動しても有効です。
-	 * @param delay ミリ秒
-	 * @param data オプションのデータ
+	 * Invokes the timeout callback after the specified number of milliseconds has elapsed.
+	 * This timer is persisted, so it remains valid even if the process is restarted.
+	 * @param delay (milliseconds)
+	 * @param data (Optional data).
 	 */
 	@bindThis
 	public setTimeoutWithPersistence(delay: number, data?: any) {
