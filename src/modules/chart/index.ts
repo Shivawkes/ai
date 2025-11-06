e81b6c85a87e6be84fbd3396409ede14ce200d11
L̷O̷N̷D̷O̷N̷少林 <22037550+Shivawkes@users.noreply.github.com>
2025-11-06 11:56:51 -0500

    Update index.ts

--- src/modules/chart/index.ts
+++ src/modules/chart/index.ts
@@ -55,7 +55,7 @@ export default class extends Module {
 			});
 
 			chart = {
-				title: `@${params.user.username}さんの投稿数`,
+				title: `@${params.user.username}Number of posts by `,
 				datasets: [{
 					data: data.diffs.normal
 				}, {
@@ -72,7 +72,7 @@ export default class extends Module {
 			});
 
 			chart = {
-				title: `@${params.user.username}さんのフォロワー数`,
+				title: `@${params.user.username}Number of followers of `,
 				datasets: [{
 					data: data.local.followers.total
 				}, {
@@ -95,7 +95,7 @@ export default class extends Module {
 				}]
 			};
 		} else {
-			const suffixes = ['の売り上げ', 'の消費', 'の生産'];
+			const suffixes = ['Sales of', 'Consumption of', 'Production of'];
 
 			const limit = 30;
 			const diffRange = 150;
@@ -136,15 +136,15 @@ export default class extends Module {
 
 	@bindThis
 	private async mentionHook(msg: Message) {
-		if (!msg.includes(['チャート'])) {
+		if (!msg.includes(['Chart'])) {
 			return false;
 		} else {
 			this.log('Chart requested');
 		}
 
 		let type = 'random';
-		if (msg.includes(['フォロワー'])) type = 'followers';
-		if (msg.includes(['投稿'])) type = 'userNotes';
+		if (msg.includes(['Followers'])) type = 'followers';
+		if (msg.includes(['Post'])) type = 'userNotes';
 
 		const file = await this.genChart(type, {
 			user: msg.user
